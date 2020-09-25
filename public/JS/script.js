479f64c5b6cd460b455d045acf218940b2cf22a5
Victor Syrkashev <victor.syrkashev@gmail.com>
2020-09-25 22:06:59 +0700

    Update 25.09 almost there

--- public/JS/script.js
+++ public/JS/script.js
@@ -4,18 +4,22 @@
   //  };
   const linkDiv = document.querySelector('.row');
   const restaurantsArray = jsonContent.restaurants;
+
   function createRestaurantsCards(restaurantsList) {
     linkDiv.innerHTML = '';
     let massiveCards = '';
     let restaurantsOnline = '';
     const restaurantsNumber = restaurantsList.length;
+
     for (let i = 0; i < restaurantsNumber; i++) {
       restaurantsOnline = '';
+
       if (restaurantsList[i].online) {
         restaurantsOnline = '<span class="badge badge-success">online</span>';
       } else {
         restaurantsOnline = '<span class="badge badge-danger">offline</span>';
       }
+
       massiveCards += `<div class = 'col mb-4'>
         <div class = 'card h-100'>
         <img style = "height: 250px; object-fit: cover" src = ${restaurantsList[i].image} class = 'card-img-top'>
@@ -30,20 +34,25 @@
         </div>
         </div>`;
     }
+
     linkDiv.innerHTML += massiveCards;
   }
+
   const hiddenBtn = document.querySelectorAll('.hidden');
   const searchRestaurant = document.querySelector('.searchRestaurant');
+
   searchRestaurant.onsubmit = function search(event) {
     event.preventDefault();
     hiddenBtn[0].hidden = true;
     hiddenBtn[1].hidden = true;
     const latitude = [];
     const longitude = [];
+
     for (let i = 0; i < restaurantsArray.length; i++) {
       latitude.push(restaurantsArray[i].location[1]);
       longitude.push(restaurantsArray[i].location[0]);
     }
+
     const maxLatitude = Math.max.apply(null, latitude);
     const minLatitude = Math.min.apply(null, latitude);
     const maxLongitude = Math.max.apply(null, longitude);
@@ -56,28 +65,36 @@
     hiddenBtn[1].hidden = false;
     const ascAlphabetSortingBtn = document.querySelector('.ascAlphSort');
     const descAlphabetSortingBtn = document.querySelector('.descAlphSort');
+    ascAlphabetSortingBtn.classList.remove('active');
+    descAlphabetSortingBtn.classList.remove('active');
     let numberOfPage = 1;
     let requestUrl = '';
-    function createListOfReasaurants(elem) {
+
+    function createListOfRestaurants(elem) {
       if (elem === undefined) {
         requestUrl = `/restaurants/search?q=${searchWords}&lat=${randomLatitude}&lon=${randomLongitude}&page=${numberOfPage}`;
       } else {
         requestUrl = `/restaurants/search?q=${searchWords}&lat=${randomLatitude}&lon=${randomLongitude}&page=${numberOfPage}` + `${elem}`;
       }
+
       const linkNav = document.querySelector('.pagination');
       linkNav.innerHTML = '';
       const request = new XMLHttpRequest();
       request.open('GET', requestUrl);
       request.responseType = 'json';
       request.send();
+
       request.onload = function () {
         const foundRestaurants = request.response;
+
         if (foundRestaurants.data.length == 0) {
           linkDiv.innerHTML = 'Unfortunately there is no such restaurant';
           return;
         }
+
         createRestaurantsCards(foundRestaurants.data);
         let navContent = '';
+
         for (let i = 1; i <= foundRestaurants.numberOfPages; i++) {
           if (i == numberOfPage) {
             navContent += `<li class='page-item active'><a class='page-link'>${i}</a></li>`;
@@ -85,28 +102,40 @@
             navContent += `<li class='page-item'><a class='page-link'>${i}</a></li>`;
           }
         }
+
         linkNav.innerHTML += navContent;
       };
     }
-    createListOfReasaurants();
+    createListOfRestaurants();
+
     ascAlphabetSortingBtn.onclick = function () {
-      createListOfReasaurants('&sort=asc');
+      event.preventDefault();
+      ascAlphabetSortingBtn.classList.add('active');
+      descAlphabetSortingBtn.classList.remove('active');
+      createListOfRestaurants('&sort=asc');
       window.history.replaceState({}, '', '/asc');
     };
+
     descAlphabetSortingBtn.onclick = function () {
-      createListOfReasaurants('&sort=desc');
+      event.preventDefault();
+      descAlphabetSortingBtn.classList.add('active');
+      ascAlphabetSortingBtn.classList.remove('active');
+      createListOfRestaurants('&sort=desc');
       window.history.replaceState({}, '', '/desc');
     };
+
     const pagination = document.getElementById('pagination');
     pagination.onclick = function (e) {
+      event.preventDefault();
       const tar = e.target;
       numberOfPage = tar.innerHTML;
-      if (ascAlphabetSortingBtn.onclick === true) {
-        createListOfReasaurants('&sort=asc');
-      } else if (descAlphabetSortingBtn.onclick === true) {
-        createListOfReasaurants('&sort=desc');
+
+      if (ascAlphabetSortingBtn.classList.contains('active')) {
+        createListOfRestaurants('&sort=asc');
+      } else if (descAlphabetSortingBtn.classList.contains('active')) {
+        createListOfRestaurants('&sort=desc');
       } else {
-        createListOfReasaurants();
+        createListOfRestaurants();
       }
     };
   };
